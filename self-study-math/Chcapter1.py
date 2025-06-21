1a5d0ffad054b33dd227d2a86bdc39c46bdffc3f
HuyDD305 <bribribridu@gmail.com>
2025-06-21 17:40:15 +0700

    study
    hehe

---
+++ self-study-math/Chcapter1.py
@@ -0,0 +1,14 @@
+# . Integers are all the positive and negative
+#  numbers on the number line, including zero, that have no fractional or decimal
+#  part. Real numbers are all the numbers on the number line, including those
+#  between the integers.
+#  I need not belabor the point; we know how to write and manipulate decimal
+#  numbers. Now, let’s take a step back and write what we mean by numbers in an
+#  arbitrary base, B
+#  …d3d2d1d0 = …+d3B3+d2B2+d1B1+d0B0
+#  where d
+# ∊
+#  [0, B – 1]. The symbol
+# ∊
+#  means that d is an element from the following
+#  set. If B = 8, then d is a number in [0,7] = {0,1,2,3,4,5,6,7}
